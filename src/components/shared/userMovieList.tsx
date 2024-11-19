d9a2e8176cbe11c7c3929fa37c6fe65108d3a577
Mariam-Lobzhanidze <mariami.lobzhanidze7@gmail.com>
2024-11-19 19:55:21 +0400

    resolved bug in movielist

--- src/components/shared/userMovieList.tsx
+++ src/components/shared/userMovieList.tsx
@@ -34,7 +34,10 @@ const UserMovieList: React.FC<UserMovieListProps> = ({ listType, title }) => {
       <div className="section-header">
         <SectionTitle title={title} count={movies?.length} />
       </div>
-      {!movies.length && (
+
+      {movies.length ? (
+        <MovieList movies={movies} isLoading={isLoading} />
+      ) : (
         <p className="no-results-message">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#ffffff" viewBox="0 0 256 256">
             <path d="M198.24,62.63l15.68-17.25a8,8,0,0,0-11.84-10.76L186.4,51.86A95.95,95.95,0,0,0,57.76,193.37L42.08,210.62a8,8,0,1,0,11.84,10.76L69.6,204.14A95.95,95.95,0,0,0,198.24,62.63ZM48,128A80,80,0,0,1,175.6,63.75l-107,117.73A79.63,79.63,0,0,1,48,128Zm80,80a79.55,79.55,0,0,1-47.6-15.75l107-117.73A79.95,79.95,0,0,1,128,208Z"></path>
@@ -42,7 +45,6 @@ const UserMovieList: React.FC<UserMovieListProps> = ({ listType, title }) => {
           {noResultsMessage}
         </p>
       )}
-      {movies.length && <MovieList movies={movies} isLoading={isLoading} />}
     </div>
   );
 };
